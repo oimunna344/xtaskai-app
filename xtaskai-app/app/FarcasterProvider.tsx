4fd108790a751c458058b035b9d7930c7f1f9179
oimunna344 <oimunna344@gmail.com>
2026-04-23 17:35:18 +0600

    add neynar score page and webhook

--- xtaskai-app/app/FarcasterProvider.tsx
+++ xtaskai-app/app/FarcasterProvider.tsx
@@ -1,28 +1,5 @@
 "use client";
 
-import { useEffect } from "react";
-import sdk from "@farcaster/frame-sdk";
-import { useConnect } from "wagmi";
-import { injected } from "wagmi/connectors";
-
 export default function FarcasterProvider({ children }: any) {
-  const { connect } = useConnect();
-
-  useEffect(() => {
-    const init = async () => {
-      try {
-        const context = await sdk.context;
-        await sdk.actions.ready();
-
-        if (context?.user?.fid) {
-          connect({ connector: injected() });
-        }
-      } catch (e) {
-        console.error("Farcaster error:", e);
-      }
-    };
-    init();
-  }, []);
-
   return children;
 }
